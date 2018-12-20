fd9f84e5999c8b3e40e42041291142d1482d6011
vvtret <tretvvmain@gmail.com>
2018-12-20 23:57:39 +0200

    Hotel overview

--- src/app.js
+++ src/app.js
@@ -4,3 +4,6 @@ require('./scss/base.scss');
 require('./img/sprite.svg');
 require('./img/IMG_0079.jpg?sizes[]=40,sizes[]=200,sizes[]=400');
 require('./img/logo.png?sizes[]=150');
+require('./img/hotel-1.jpg?sizes[]=330,sizes[]=660');
+require('./img/hotel-2.jpg?sizes[]=330,sizes[]=660');
+require('./img/hotel-3.jpg?sizes[]=330,sizes[]=660');
